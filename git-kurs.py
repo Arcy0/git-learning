da8ac6319f8f8e6cd3f047f5bd86259a8687fdbe
Krzysztof <krzys.zolnowski@gmail.com>
2022-07-10 23:58:14 +0200

    Nowa zmiana

--- git-kurs.py
+++ git-kurs.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-print("Hello Mariusz")
-print("Zmiana w drugim repo")
-=======
-<<<<<<< HEAD
-print("Hello Dominik")
-print("Zmiana w drugim repo")
-=======
-print("Hello Poland")
->>>>>>> 2feab8a (Zmiana Hello World)
->>>>>>> 88049ab (Hello Dominik)
+print('Wszystko usubnięte')
+
